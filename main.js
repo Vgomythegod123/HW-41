a4fe885ce2b81092b3dc648120a0ff0fcc81681f
Vgomythegod123 <114121256+Vgomythegod123@users.noreply.github.com>
2022-09-21 21:24:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Lukaheading(){
+document.getElementById("Luka").style.color="white";
+}
+
+function image(){
+    document.getElementById("image").src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3945274.png&w=350&h=254";
+}
